9cc04ca0e2ce142f7245344032646b1c92ea4e86
geeks18 <46871822+geeks18@users.noreply.github.com>
2019-01-24 10:58:29 +0000

    project

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into user (user_id, name,password,role) values ('joy','Joydip','123','ROLE_USER')
+insert into user (user_id, name,password,role) values ('admin','admin','admin','ROLE_ADMIN')
